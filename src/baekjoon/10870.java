301867b4af6b75180ad6d60dfa29cbcda799849c
Bomin Jang <44834680+bominjang@users.noreply.github.com>
2021-01-20 23:49:40 +0900

    Create 10870.java

---
+++ src/baekjoon/10870.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N  = Integer.parseInt(br.readLine());
+
+        System.out.println(fibo(N));
+    }
+
+    public static int fibo(int N){
+        if(N==0) return 0;
+        if(N==1) return 1;
+        return fibo(N-1)+fibo(N-2);
+    }
+
+}
+
+// Scanner보다 BufferedReader가 시간이 적게 걸림.
+// 피보나치는 재귀말고 for문으로도 구현할 수 있음
