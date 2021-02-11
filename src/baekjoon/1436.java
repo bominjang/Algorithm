ca69ba87fd65351a0052c90743f134c5fbfa1a8d
Bomin Jang <44834680+bominjang@users.noreply.github.com>
2021-02-11 23:19:44 +0900

    Update 1436.java

--- src/baekjoon/1436.java
+++ src/baekjoon/1436.java
@@ -1,22 +1,26 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
- 
-		int num = 666;
-		int count = 1;
-        
-		while(count != N) {
-			num++;
-			if(String.valueOf(num).contains("666")) {
-				count++;
-			}
-		}
-		System.out.println(num);
-	}
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while(count!=N){
+            num++;
+
+            if(String.valueOf(num).contains("666")){
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
 }
+
+/*
+@author kdgyun
+*/
