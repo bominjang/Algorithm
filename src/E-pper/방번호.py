d3dc5f7d0ea1456c001fe5cc2ca9c199586c5360
Bomin Jang <44834680+bominjang@users.noreply.github.com>
2021-03-14 22:23:33 +0900

    Update 방번호.py

--- src/E-pper/방번호.py
+++ src/E-pper/방번호.py
@@ -11,3 +11,17 @@ def solution():
     
     print(room + " " + number)
         
+#==============================================
+
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+from pip._vendor.distlib.compat import raw_input
+
+n = int(raw_input())
+room_n = 1+n//15
+in_n = n%15
+if(in_n==0):
+    room_n = n//15
+    in_n = 15
+
+print("%d %d" %(room_n, in_n))
