093e59353d5a729ce246c54194ff438b5578c5df
Bomin Jang <44834680+bominjang@users.noreply.github.com>
2021-03-15 23:34:55 +0900

    Update 회문수정.py

--- src/E-pper/회문수정.py
+++ src/E-pper/회문수정.py
@@ -1,18 +1,29 @@
 def solution(arr, start, end):
-    cnt = 0
-    
-    if(start == end or start>end):
+    global cnt
+    if (start == end or start > end):
         return cnt
-    
-    # 배열 원소 비교
-    if(arr[start]==arr[end]):
-        return (solution(arr, start+1, end-1))
+    if (arr[start] == arr[end]):
+        return solution(arr, start + 1, end - 1)
     else:
-        if(arr[start]<arr[end]):
-            arr[start+1] = arr[start]+arr[start+1]
+        if (arr[start] < arr[end]):
             cnt += 1
-            return solution(arr, start+1, end)
+            arr[start + 1] = arr[start] + arr[start + 1]
+            return solution(arr, start + 1, end)
         else:
-            arr[end-1] = arr[end]+arr[end-1]
             cnt += 1
-            return solution(arr, start, end-1)
+            arr[end - 1] = arr[end] + arr[end - 1]
+            return solution(arr, start, end - 1)
+
+    return answer
+
+
+n = int(input())
+arr = input().split(" ")
+for i in range(n):
+    arr[i] = int(float(arr[i]))
+
+start = int(0);
+end = int(n - 1);
+cnt = 0
+
+print(solution(arr, start, end))
