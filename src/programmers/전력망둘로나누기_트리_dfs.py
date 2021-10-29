e80c7c07effd977a4e82f1b6c72da86abc01a6c3
Bomin Jang <44834680+bominjang@users.noreply.github.com>
2021-10-29 15:11:05 +0900

    Update 전력망둘로나누기_트리_dfs.py

--- src/programmers/전력망둘로나누기_트리_dfs.py
+++ src/programmers/전력망둘로나누기_트리_dfs.py
@@ -14,17 +14,23 @@ def solution(n, wires):
     diff=[] #차이들을 저장
     tree = defaultdict(list)#default값이 list인 딕셔너리 선언. 인접리스트
     
+    #트리 만들기
     for a, b in wires:
         tree[a].append(b)
         tree[b].append(a)
     #전선 하나씩 끊으면서 돌기
     for a, b in wires:
+        #끊고
         tree[a].remove(b)
         tree[b].remove(a)
+        #count 0으로 만들어주고
         count=0
+        #개수세기. 출발
         dfs(1, [])
         #차이를 넣기
         diff.append(abs(n-2*count))
+        #다시 전선 연결하기
         tree[a].append(b)
         tree[b].append(a)
+    #차이들 중 최소값을 return
     return min(diff)
